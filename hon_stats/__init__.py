067aaeb752d88714d6dabdca44bdae085060ab27
Sean Quinn <swquinn@gmail.com>
2019-10-01 14:57:03 -0400

    Rework the stats plugin code organization

---
+++ hon_stats/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+    hon_stats
+    ~~~~
+
+    :copyright: © 2019 by Sean Quinn.
+    :license: MIT, see LICENSE for more details.
+"""
+from .stats import HonStats
+
+__version__ = '1.0.0'
---
+++ hon_stats/stats.py
@@ -0,0 +1,16 @@
+"""
+    hon_stats.stats
+    ~~~~~
+
+    A plugin for Hon. It provides insight into a book, how many words, pages,
+    etc.
+
+    :license: MIT, see LICENSE for more details.
+"""
+import hon
+from hon.plugins import Plugin
+
+
+class HonStats(Plugin):
+    _name = 'HonStats'
+    pass
